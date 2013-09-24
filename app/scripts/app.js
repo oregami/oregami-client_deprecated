d95317c3be66340f40299083b7454560ef22f347
kultpower <gene@kultpower.de>
2013-09-24 16:35:52 +0200

    navigation & config

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -3,7 +3,8 @@
 var app = angular.module('oregamiClientApp', [])
 
     .constant('mySettings', {
-        apiUri: 'http://localhost:9003',
+        //apiUri: 'http://localhost:8080',
+        apiUri: 'http://test.server.oregami.org:6080',
         version: '0.0.1'
     })
 
@@ -33,9 +34,33 @@ var app = angular.module('oregamiClientApp', [])
         templateUrl: 'views/magazines.html',
         controller: 'MagazinesCtrl'
       })
+      .when('/config', {
+        templateUrl: 'views/config.html',
+        controller: 'ConfigCtrl'
+      })
       .otherwise({
         redirectTo: '/'
       });
   })
 
 ;
+
+
+function getCookie(name) {
+    var nameEQ = name + "=";
+    var ca = document.cookie.split(';');
+    for(var i=0;i < ca.length;i++) {
+        var c = ca[i];
+        while (c.charAt(0)==' ') c = c.substring(1,c.length);
+        if (c.indexOf(nameEQ) == 0) return unescape(c.substring(nameEQ.length,c.length));
+    }
+    return null;
+}
+
+function setCookie(name,value,exdays)
+{
+    var exdate=new Date();
+    exdate.setDate(exdate.getDate() + exdays);
+    var value=escape(value) + ((exdays==null) ? "" : "; expires="+exdate.toUTCString());
+    document.cookie=name + "=" + value;
+}
