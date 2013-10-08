7af79ea0c57e1f2878e375599b2f074f45696e32
kultpower <gene@kultpower.de>
2013-10-08 21:23:36 +0200

    custom url

--- app/scripts/services/gamesService.js
+++ app/scripts/services/gamesService.js
@@ -10,6 +10,9 @@ angular.module('oregamiClientApp')
             getGames: function() {
                 // $http returns a promise, which has a then function, which also returns a promise
                 var url = $mySettings.apiUri + '/games';
+                if (getCookie("customUrl")!=null) {
+                    url = getCookie("customUrl") + '/games'
+                }
                 console.log('gamesService.test() called => ' + url);
                 var promise = $http.get(url).then(function (response) {
                     // The then function here is an opportunity to modify the response
@@ -19,7 +22,48 @@ angular.module('oregamiClientApp')
                 });
                 // Return the promise to the controller
                 return promise;
+            } ,
+/*
+            updateGame: function(game) {
+                var url = $mySettings.apiUri + '/games';
+                if (game.id==null) {
+                    $http({
+                        url: url,
+                        method: "POST",
+                        data: game,
+                        headers: {'Content-Type': 'application/json'}
+                    }).success(function (data, status, headers, config) {
+                            //$scope.persons = data; // assign  $scope.persons here as promise is resolved here
+                            console.log("post / insert / success");
+                        }).error(function (data, status, headers, config) {
+                            //$scope.status = status;
+                            console.log("post / insert / error");
+                        });
+                } else {
+                    $http({
+                        url: url,
+                        method: "PUT",
+                        data: game,
+                        headers: {
+                            //'Content-Type': 'application/json',
+                            //'Access-Control-Allow-Headers': 'Access-Control-Allow-Origin',
+                            //'Access-Control-Allow-Origin' : '*'
+                            'Access-Control-Allow-Origin': '*',
+                            'Access-Control-Allow-Headers': 'Content-Type, X-Requested-With, Access-Control-Allow-Methods, Access-Control-Allow-Origin',
+                            'Access-Control-Allow-Methods': 'GET, POST, OPTIONS, PUT',
+                            'Content-Type': 'application/json'
+                        }
+                    }).success(function (data, status, headers, config) {
+                            //$scope.persons = data; // assign  $scope.persons here as promise is resolved here
+                            console.log("put / update / success");
+                        }).error(function (data, status, headers, config) {
+                            //$scope.status = status;
+                            console.log("put / update / error");
+                        });
+                }
             }
+ */
         }
+
 
   }]);
