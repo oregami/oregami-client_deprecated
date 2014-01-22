7aab389dde8f77603e3ec4fa7e81571ed58c07ce
kultpower <gene@kultpower.de>
2014-01-22 07:15:04 +0100

    removed unused code

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -76,23 +76,3 @@ app.config(['$translateProvider', '$translatePartialLoaderProvider', function ($
 
     $translateProvider.preferredLanguage('en');
 }]);
-
-
-function getCookie(name) {
-    var nameEQ = name + "=";
-    var ca = document.cookie.split(';');
-    for(var i=0;i < ca.length;i++) {
-        var c = ca[i];
-        while (c.charAt(0)==' ') c = c.substring(1,c.length);
-        if (c.indexOf(nameEQ) == 0) return unescape(c.substring(nameEQ.length,c.length));
-    }
-    return null;
-}
-
-function setCookie(name,value,exdays)
-{
-    var exdate=new Date();
-    exdate.setDate(exdate.getDate() + exdays);
-    var value=escape(value) + ((exdays==null) ? "" : "; expires="+exdate.toUTCString());
-    document.cookie=name + "=" + value;
-}
