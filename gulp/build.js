8416047a21994725af1106d18dd5d0179ae426de
sebeichholz <gene@oregami.org>
2015-02-20 21:40:07 +0100

    gulp task "language"

--- gulp/build.js
+++ gulp/build.js
@@ -67,6 +67,11 @@ gulp.task('images', function () {
     .pipe(gulp.dest(paths.dist + '/assets/images/'));
 });
 
+gulp.task('language', function () {
+  return gulp.src(paths.src + '/assets/language/**/*')
+    .pipe(gulp.dest(paths.dist + '/assets/language/'));
+});
+
 gulp.task('fonts', function () {
   return gulp.src($.mainBowerFiles())
     .pipe($.filter('**/*.{eot,svg,ttf,woff}'))
@@ -83,4 +88,4 @@ gulp.task('clean', function (done) {
   $.del([paths.dist + '/', paths.tmp + '/'], done);
 });
 
-gulp.task('build', ['html', 'images', 'fonts', 'misc']);
+gulp.task('build', ['html', 'images', 'language', 'fonts', 'misc']);
