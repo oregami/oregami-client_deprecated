e808fca0de2af6075db724dc46ce5f42f6489d7c
sebeichholz <gene@oregami.org>
2015-02-20 22:10:40 +0100

    navigation, footer

---
+++ src/app/navigation/navigation.js
@@ -0,0 +1,30 @@
+'use strict';
+
+angular.module('oregamiClientApp')
+  .controller('NavigationCtrl',
+            ['$scope', '$location', '$route', '$translate', '$translatePartialLoader',
+    function ($scope,   $location,   $route, $translate,     $translatePartialLoader) {
+
+    $translatePartialLoader.addPart('navigation');
+
+    $scope.navlist = [
+        //{name: "home", url: "", title:"home of the site"},
+        {name: "publications", url: "publications", title: 'show publications'},
+        {name: "games", url: "games", title: "list all games"},
+        {name: "gameTitles", url: "gameTitles", title: 'gameTitles'},
+        {name: "regions", url: "regions", title: 'regions'},
+        {name: "websites", url: "websites", title: 'websites'},
+        {name: "persons", url: "persons", title: 'all persons'},
+        {name: "companies", url: "companies", title: 'view all companies'},
+
+    ];
+
+    $scope.isActive = function(route) {
+        return route === $location.path();
+    }
+
+    $scope.$location = $location;
+    $scope.$route = $route;
+
+
+  }]);
