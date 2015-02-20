d7c40ea6ce3420555df8e29df4a5688991904bb4
sebeichholz <gene@oregami.org>
2015-02-20 22:43:58 +0100

    fixed gameTitleFiltering

--- src/app/games/games.js
+++ src/app/games/games.js
@@ -2,28 +2,35 @@
 
 angular.module('oregamiClientApp')
   .controller('GamesCtrl', ['$scope', 'gamesService', '$translate', '$translatePartialLoader', 'Restangular',
-                   function ($scope,  $gamesService,   $translate,   $translatePartialLoader,   Restangular) {
+    function ($scope, $gamesService, $translate, $translatePartialLoader, Restangular) {
 
-    $translatePartialLoader.addPart('games');
+      $translatePartialLoader.addPart('games');
 
-    $scope.getGames = function() {
-        $gamesService.getGames().then(function(d) {
-            $scope.games = d;
+      $scope.getGames = function () {
+        $gamesService.getGames().then(function (d) {
+          $scope.games = d;
         });
-    };
+      };
 
-    //load games:
-    $scope.getGames();
+      //load games:
+      $scope.getGames();
 
-    $scope.game=null;
+      $scope.originalTitleFilter = function (gameToGameTitleConnection) {
+        return gameToGameTitleConnection.titleType.value == 'ORIGINAL_TITLE';
+      };
+      $scope.notOriginalTitleFilter = function (gameToGameTitleConnection) {
+        return !$scope.originalTitleFilter(gameToGameTitleConnection);
+      };
 
-        /*
-        $scope.updateGame = function(game) {
-            $scope.game= angular.copy(game);
-            console.log($scope.game);
-            $gamesService.updateGame(game);
-        };
-        */
+      $scope.game = null;
+
+      /*
+       $scope.updateGame = function(game) {
+       $scope.game= angular.copy(game);
+       console.log($scope.game);
+       $gamesService.updateGame(game);
+       };
+       */
 
 
-  }]);
+    }]);
